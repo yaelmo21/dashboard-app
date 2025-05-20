25b877a068a512accd77ba13adc47810d49f279f
Yael Ruiz <yaelruiz99@outlook.com>
2025-05-20 10:33:39 -0600

    Fix: Content Screen layout

--- presentation/components/dashboard/SidebarLayout.tsx
+++ presentation/components/dashboard/SidebarLayout.tsx
@@ -4,12 +4,12 @@ import MobileNavbar from './MobileNavbar';
 
 const SidebarLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className='flex flex-row h-full'>
+    <div className='flex flex-row h-full w-screen'>
       <Navbar />
       <MobileNavbar />
       <div>
-        <main className='py-10'>
-          <div className='px-4 sm:px-6 lg:px-8 pt-[30px] sm:pt-0 md:pl-[80px] lg:pl-[260px]'>
+        <main className='py-10 '>
+          <div className='px-4 sm:px-6 lg:px-8 pt-[30px] sm:pt-0 md:pl-[80px] lg:pl-[260px] w-screen'>
             {children}
           </div>
         </main>
