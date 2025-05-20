f71f0472043fe0b342ace1477c1c41269129218f
Yael Ruiz <yaelruiz99@outlook.com>
2025-05-19 19:14:58 -0600

    Style update color muted

--- presentation/components/auth/LoginForm.tsx
+++ presentation/components/auth/LoginForm.tsx
@@ -6,12 +6,16 @@ import Image from 'next/image';
 const LoginForm = () => {
   return (
     <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-full max-w-2xl'>
-      <div>
+      <div className='flex flex-col gap-3 items-center justify-center mb-4'>
         <Image
           src={logo}
           alt='Logo Dashboard Kit'
-          className='mx-auto mb-4 w-12 h-12'
+          className='w-12 h-12'
         />
+
+        <h1 className='text-2xl font-bold text-center text-muted'>
+          Dashboard Kit
+        </h1>
       </div>
     </div>
   );
